bfd7ce534690ac3e7764071373242a457eb6b83a
nishp763 <nishp763@gmail.com>
2022-04-09 11:18:55 -0700

    finished testing slam with my own world file

--- Project5/scripts/test_slam.sh
+++ Project5/scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../world/minimap.world" &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
